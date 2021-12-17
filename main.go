3997695f1cb49060cf19bdc597fa9e9b6a1214a2
Anton Ždanov <azdanov@pm.me>
2021-12-17 16:46:46 +0200

    add routing

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Scratchpad!"))
+}
+
+func showPad(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific scratchpad..."))
+}
+
+func createPad(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new scratchpad..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/pad", showPad)
+	mux.HandleFunc("/pad/create", createPad)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
