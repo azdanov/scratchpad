9dc7fbc98d18a938a74dfc4e4e3d0e4ad5811322
Anton Ždanov <azdanov@pm.me>
2022-01-11 20:49:33 +0200

    add form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/azdanov/scratchpad/pkg/models"
 )
@@ -57,6 +59,32 @@ func (app *application) createScratchpad(w http.ResponseWriter, r *http.Request)
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
+
+	formErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		formErrors["title"] = "Title cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		formErrors["title"] = "Title is too long (maximum is 100 characters)"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		formErrors["content"] = "Content cannot be blank"
+	}
+
+	if strings.TrimSpace(expires) == "" {
+		formErrors["expires"] = "Expires cannot be blank"
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		formErrors["expires"] = "Expires is invalid"
+	}
+
+	if len(formErrors) > 0 {
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: formErrors,
+			FormData:   r.PostForm,
+		})
+		return
+	}
 
 	id, err := app.scratches.Insert(title, content, expires)
 	if err != nil {
